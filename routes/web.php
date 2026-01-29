b86e367cfb0da1793a16ea0c5397a576bf443946
ReyhanMFSoul <2307001@itg.ac.id>
2026-01-29 16:09:39 +0700

    update css lagi

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProkerController;
-use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Menghubungkan URL /proker ke ProkerController
+
 Route::resource('proker', ProkerController::class);
